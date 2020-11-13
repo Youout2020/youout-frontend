6bf76270dc66eb0162189cbeebebe145e1a33aca
JIHYUNHA <humm223@gmail.com>
2020-11-13 21:37:40 +0900

    style: Code formatting

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import styles from './GameRoom.module.scss';
-// import { useLazyObserver } from '../utils/hooks';
 
 const GameRoom = ({ isPlaying, name, users, setTarget }) => {
-  //TODO: users.length 들어올 때마다 업데이트 필요하여 socket으로 통제
-  //TODO: lazyLoading const { roomRef } = useLazyObserver();
-
   return (
     <div className={styles.container} ref={setTarget}>
       <div className={styles.status}>
