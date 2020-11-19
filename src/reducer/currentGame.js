c50613c247672b5e5f0cae381cf26aca48b988a4
JIHYUNHA <humm223@gmail.com>
2020-11-19 10:05:36 +0900

    fix: Modify currentGame initstate

--- src/reducer/currentGame.js
+++ src/reducer/currentGame.js
@@ -4,7 +4,22 @@ export const UPDATE_CURRENT_GAME = 'currentGameReducer/UPDATE_CURRENT_GAME';
 
 export const updateCurrentGame = createAction(UPDATE_CURRENT_GAME);
 
-const initState = null;
+const initState = {
+  gameInfo: {
+    _id: '',
+    name: '',
+    owner: '',
+    quizList: [],
+    timeLimit: 0,
+    location: {},
+    address: '',
+    addressDetail: '',
+    createdAt: '',
+    updatedAt: '',
+  },
+  isPlaying: false,
+  users: [],
+};
 
 export default createReducer(initState, {
   [UPDATE_CURRENT_GAME]: (state, action) => {
