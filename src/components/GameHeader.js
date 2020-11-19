cf9d95bde4fb109d0a78306c56d1a6767e0a56c0
JIHYUNHA <humm223@gmail.com>
2020-11-19 10:01:24 +0900

    feat: Add game header features

--- src/components/GameHeader.js
+++ src/components/GameHeader.js
@@ -1,5 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './GameHeader.module.scss';
+import { ExitCard, HintCard } from './Card';
 
 const GameHeader = ({
   minutes,
@@ -8,7 +9,19 @@ const GameHeader = ({
   setSeconds,
   children,
   currentHint,
+  gamePhase,
 }) => {
+  const [isHintShowing, setIsHintShowing] = useState(false);
+  const [isExistShowing, setIsExitShowing] = useState(false);
+
+  const handleHintClick = () => {
+    setIsHintShowing(!isHintShowing);
+  };
+
+  const handleExitClick = () => {
+    setIsExitShowing(!isExistShowing);
+  };
+
   useEffect(() => {
     const timerId = setTimeout(() => {
       if (seconds > 0) {
@@ -18,24 +31,52 @@ const GameHeader = ({
       if (seconds === 0) {
         if (minutes === 0) {
           //TODO: 게임 종료 알림 (Socket, container에서 내려주기)
-          return () => clearTimeout(timerId);
+          clearTimeout(timerId);
+          return;
         }
-
         setMinutes((prev) => prev - 1);
         setSeconds(59);
       }
     }, 1000);
+
+    return () => clearTimeout(timerId);
   }, [seconds]);
 
   return (
     <div className={styles.container}>
       <div className={styles.buttons}>
-        <div>힌트</div>
-        <div>{`${minutes}:${seconds}`}</div>
-        <div>종료</div>
+        <div onClick={handleHintClick}>힌트</div>
+        <div>
+          {
+            minutes < 10 ? `0${minutes}` : minutes
+          }:{
+            seconds < 10 ? `0${seconds}` : seconds
+          }
+        </div>
+        <div onClick={handleExitClick}>종료</div>
       </div>
       <div className={styles.contents}>
         {children}
+        {
+          isHintShowing
+          &&
+          <HintCard
+            text={
+              gamePhase === 'quiz'
+                ? currentHint
+                : '아직 기다려요!'
+            }
+            onClick={handleHintClick}
+          />
+        }
+        {
+          isExistShowing
+          &&
+          <ExitCard
+            text='정말 종료할건가요?🧨'
+            onClick={handleExitClick}
+          />
+        }
       </div>
     </div>
   );
